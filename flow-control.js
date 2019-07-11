cb68b03899b781319a788497f5842ec31d91dcda
Thomas Varo <zalipsn@gmail.com>
2019-07-11 11:42:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,13 +23,13 @@ function ternaryTeenager(age) {return age >= 13 && age <=19 ? 'You are a teenage
 
 function switchAge(age) {
   switch(age) {
-  case "13":
-  case "14":
-  case "15":
-  case "16":
-  case "17":
-  case "18":
-  case "19":
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
   return 'You are a teenager';
   break;
   default:
